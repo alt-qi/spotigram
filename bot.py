a51c83b9dc82f23f50f5061640e499b259a96463
alt-qi <a-ti-radeon@yandex.ru>
2022-01-29 15:22:25 +0700

    Spotify authorization feature is finished

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from asyncio import exceptions
 from psycopg2 import OperationalError
 from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
@@ -26,16 +27,15 @@ port = "INSERT DATABASE PORT HERE"
     print("I have created a \".env\" file, please fill required fields in it.")
     exit()
 
-try:
-    db.init()
-except OperationalError:
-    print("Failed to connect to database! Please make sure, that database creditionals stored in \".env\" are valid.")
-
 load_dotenv(".env")
 
 try:
+    db.init()
     bot = Bot(os.getenv("bot_token"))
-except:
+except OperationalError:
+    print("Failed to connect to database! Please make sure, that database creditionals stored in \".env\" are valid.")
+    exit()
+except exceptions.ValidationError:
     print("Bot failed to start! Please make sure, that token stored in \".env\" file is valid.")
     exit()
 
@@ -45,17 +45,38 @@ dp = Dispatcher(bot)
 async def welcome(message: types.Message):
     await message.answer(f"Hello, `{message.from_user.full_name}`\!", parse_mode="MarkdownV2")
 
-@dp.message_handler(commands=["generateurl"])
-async def generate_url(message: types.Message):
-    auth_code = spotify.generate_code()
-    keyboard = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton(
-        text="Auth URL",
-        url=spotify.generate_url(auth_code)
-    )
-    keyboard.add(button)
-    db.save_auth_code(message.from_user.id, auth_code)
-    await message.answer("Your link is ready!", reply_markup=keyboard)
+@dp.message_handler(commands=["auth"])
+async def auth(message: types.Message):
+    if not db.user_token_exists(message.from_user.id):
+        auth_code = spotify.generate_code()
+        keyboard = types.InlineKeyboardMarkup()
+        button = types.InlineKeyboardButton(
+            text="Authorize Spotify",
+            url=spotify.generate_url(auth_code)
+        )
+        keyboard.add(button)
+        db.save_auth_code(message.from_user.id, auth_code)
+        await message.answer("Click the button below to log into Spotify\.", reply_markup=keyboard, parse_mode="MarkdownV2")
+    else:
+        await message.answer(
+            "*You already logged in\.*\n\n" \
+            "If you want to reauthenticate, first use /logout", parse_mode="MarkdownV2")
+
+@dp.message_handler(commands=["logout"])
+async def logout(message: types.Message):
+    if db.user_token_exists(message.from_user.id):
+        db.remove_spotify_token(message.from_user.id)
+        await message.answer(
+            "*Auth information has been deleted*\.\n\n" \
+            "If you want to start using the bot again, you will need to authenticate again with /auth", parse_mode="MarkdownV2")
+    else:
+        await message.answer(
+            "*You are not authorized yet\.*\n\n" \
+            "If you want to log in, use /auth", parse_mode="MarkdownV2")
+
+# @dp.inline_handler()
+# async def inline_echo(inline_query: types.InlineQuery):
+#     ...
 
 if __name__ == '__main__':
     executor.start_polling(dp)
--- utils/db.py
+++ utils/db.py
@@ -14,19 +14,20 @@ con = psycopg2.connect(**db_config)
 cur = con.cursor()
 
 def create_table() -> None:
-    try:
-        cur.execute("CREATE TABLE auth (user_id INTEGER, code TEXT)")
-    except:
-        ...
+    try: cur.execute("CREATE TABLE spotify_tokens (user_id INTEGER, token TEXT)")
+    except: ...
+    con.commit()
+    try: cur.execute("CREATE TABLE auth (user_id INTEGER, code TEXT)")
+    except: ...
     con.commit()
 
-def reset_table() -> None:
+def reset_auth_links() -> None:
     cur.execute("DELETE FROM auth")
     con.commit()
 
 def init() -> None:
     create_table()
-    reset_table()
+    reset_auth_links()
 
 def user_exists(user_id: int) -> bool:
     cur.execute("SELECT EXISTS (SELECT * FROM auth WHERE user_id = %s)", (user_id,))
@@ -37,4 +38,12 @@ def save_auth_code(user_id: int, code: str) -> None:
         cur.execute("INSERT INTO auth VALUES (%s, %s)", (user_id, code))
     else:
         cur.execute("UPDATE auth SET code = %s WHERE user_id = %s", (code, user_id))
+    con.commit()
+
+def user_token_exists(user_id: int) -> bool:
+    cur.execute("SELECT EXISTS (SELECT * FROM spotify_tokens WHERE user_id = %s)", (user_id,))
+    return cur.fetchall()[0][0]
+
+def remove_spotify_token(user_id: int) -> None:
+    cur.execute("DELETE FROM spotify_tokens WHERE user_id = %s", (user_id,))
     con.commit()
